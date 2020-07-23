b09066dd37649194240bd7b9b6cef2777d3fb39a
myoung hye yeon <hyeyeon2964@hanmail.net>
2020-07-24 06:49:54 +0900

    c08

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -6,19 +6,24 @@
 /*   By: hmyoung <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/21 20:36:03 by hmyoung           #+#    #+#             */
-/*   Updated: 2020/07/23 11:27:18 by hmyoung          ###   ########.fr       */
+/*   Updated: 2020/07/23 15:41:39 by hmyoung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
-# define TRUE 1
+
+# include <unistd.h>
+
 # define FALSE 0
-
-typedef int		t_bool;
-
-# define EVEN(x) (x % 2 == 0)
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define TRUE 1
 # define SUCCESS 0
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+typedef	int		t_bool;
+
+# define EVEN(number) (number % 2 == 0)
+
 #endif
