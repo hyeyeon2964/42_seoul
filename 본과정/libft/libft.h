170d45a454ecfe826304325928a36c16d18e7a3b
myoung hye yeon <hyeyeon2964@hanmail.net>
2020-10-30 17:04:32 +0900

    ft_strchr.c

--- 본과정/libft/libft.h
+++ 본과정/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hmyoung <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 19:57:26 by hmyoung           #+#    #+#             */
-/*   Updated: 2020/10/28 21:06:49 by hmyoung          ###   ########.fr       */
+/*   Updated: 2020/10/28 21:46:57 by hmyoung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ void	*ft_memcpy(void *des, const void *src, size_t num);
 void	*ft_memccpy(void *des, const void *src, int c, size_t num);
 void	*ft_memmove(void *des, const void *src, size_t num);
 void	*ft_memchr(void *ptr, int value, size_t num);
-int		*ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
+int		ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
 #endif
