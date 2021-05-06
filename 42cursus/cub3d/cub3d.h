0ed880ed6521429ffe0edf0215990da94e5eae0a
myoung hye yeon <hyeyeon2964@hanmail.net>
2021-05-06 17:44:23 +0900

    cub3d clone study

--- 42cursus/cub3d/cub3d.h
+++ 42cursus/cub3d/cub3d.h
@@ -1,23 +1,61 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "./mlx/mlx.h"
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <math.h>
-# include <fcntl.h>
+# include "engine/engine.h"
+# include "config/config.h"
+# include "utils/utils.h"
 
-# define OPEN_MAX 1024
-# define BUFFER_SIZE 5
-# define MAX 32
+# define LEFT_CLICK 	1
+# define RIGHT_CLICK 	2
+# define MIDDLE_CLICK	3
+# define SCROLL_UP		4
+# define SCROLL_DOWN	5
 
-int		get_next_line(int fd, char **line);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
+# define KEY_1			18
+# define KEY_2			19
+# define KEY_3			20
+# define KEY_4			21
+# define KEY_Q			12
+# define KEY_W			13
+# define KEY_E			14
+# define KEY_R			15
+# define KEY_A			0
+# define KEY_S			1
+# define KEY_D			2
+# define KEY_I			34
+# define KEY_O			31
+# define KEY_P			35
+# define KEY_J			38
+# define KEY_K			40
+# define KEY_L			37
+# define KEY_LEFT		123
+# define KEY_RIGHT		124
+# define KEY_FORWARD 	126
+# define KEY_BACKWARD	125
+
+# define KEY_TAB	48
+# define KEY_MAJ	257
+# define KEY_ESC	53
+# define KEY_TAB	48
+
+int				exit_error(t_game *game, char const *str);
+
+void			init_game(t_game *game, int save_opt);
+
+int				finish_init(t_game *game);
+
+int				exit_game(t_game *game, int code);
+
+int				screenshot(t_game *game);
+
+int				exit_hook(t_game *game);
+
+int				key_press(int keycode, t_game *game);
+
+int				key_release(int keycode, t_game *game);
+
+int				main_loop(t_game *game);
+
+int				save_bmp(t_game *game);
 
 #endif
--- 42cursus/cub3d/main.c
+++ 42cursus/cub3d/main.c
@@ -1,15 +1,31 @@
+#include <stdlib.h>
 #include "cub3d.h"
 
-int main(int argc, char **argv)
+int	exit_error(t_game *game, char const *str)
 {
-	int fd;
-	int ret;
-	char *line;
-	int err;
+	if (str)
+		write(STDOUT_FILENO, str, ft_strlen(str));
+		exit_game(game, EXIT_FAILURE);
+		return (EXIT_FAILURE);
+}
 
-	err = 0;
-	fd = open(argv[1], O_RDONLY);
-	ret = get_next_line(fd, &line);
-	printf("%s", line);
+int	main(int argc, char **argv)
+{
+	t_game game;
+	int save_opt;
 
+	save_opt = (argc >= 2 && !ft_strcmp(argv[1], "-save"));
+	if (argc < (2 + save_opt))
+		return (sxit_error(&game, "Error:\n savq option error \n"));
+	init_game(&game, save_opt);
+	if (!parse_config(&game.config, argv[1 + save_opt]))
+		return (EXIT_FAILURE);
+	if (game.option & FLAG_SAVE)
+		return (screenshot(&game));
+	mlx_hook(game.window.win, X_EVENT_KEY_PRESS, 0, &key_press, &game);
+	mlx_hook(game.window.win, X_EVENT_KEY_RELEASE, 0, &key_release, &game);
+	mlx_hook(game.window.win, X_EVENT_EXIT, 0, &exit_hook, &game);
+	mlx_loop_hook(game.window.ptr, &main_loop, &game);
+	mlx_loop(game.window.ptr);
+	return (EXIT_SUCCESS);
 }
